3d6084be70b6342906ea44fff7323b3c444b982d
foureverhh <foureverhh@gmail.com>
2018-10-29 20:40:10 +0100

    Added camera source, add processor to textReconzier

--- app/src/main/java/com/example/zfgg04/androidcamerareconitiontext/MainActivity.java
+++ app/src/main/java/com/example/zfgg04/androidcamerareconitiontext/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.zfgg04.androidcamerareconitiontext;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +24,25 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     CameraSource cameraSource;
     final int RequestCameraPermissionID = 1001;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case RequestCameraPermissionID:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +56,63 @@ public class MainActivity extends AppCompatActivity {
         TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
 
         //Set CameraSource to transfer info from camera to detector
-        cameraSource = new CameraSource.Builder(getApplicationContext(),textRecognizer)
+        cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
                 .setFacing(CameraSource.CAMERA_FACING_BACK)
-                .setRequestedPreviewSize(1280,1024)
+                .setRequestedPreviewSize(1280, 1024)
                 .setRequestedFps(2.0f)
                 .setAutoFocusEnabled(true)
                 .build();
 
+        //Set surface view
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},RequestCameraPermissionID);
+                    return;
+                }
+                try {
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                final SparseArray<TextBlock> items = detections.getDetectedItems();
+                if(items.size() != 0){
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            for(int i = 0; i < items.size(); i++){
+                                TextBlock item = items.valueAt(i);
+                                stringBuilder.append(item.getValue());
+                                stringBuilder.append("\n");
+                            }
+                            textView.setText(stringBuilder.toString());
+                        }
+                    });
+                }
+            }
+        });
     }
 }
